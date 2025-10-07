08f60fc75bcb945d91f3f0ceeaac952defe182e7
divine2192 <divine.ede@yahoo.com>
2025-10-07 17:21:52 +0100

    update images

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-
 import MovieDetail from "./pages/MovieDetail";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Account from "./components/Account";
 import Footer from "./components/Footer";
 
-
 const App = () => {
   return (
-     <BrowserRouter basename="/christian-movie-app">
-
-  
-    <Router>
+    <BrowserRouter basename="/christian-movie-app">
       <Header />
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<MovieDetail />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} /> {/* Add the Login route */}
+        <Route path="/login" element={<Login />} />
         <Route path="/account" element={<Account />} />
-
       </Routes>
-         <Footer />
-    </Router>
-       </BrowserRouter>
+
+      <Footer />
+    </BrowserRouter>
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/christian-movie-app/', // 👈 important for GitHub Pages
+  base: '/christian-movie-app/', // 👈 your GitHub repo name here
 })
